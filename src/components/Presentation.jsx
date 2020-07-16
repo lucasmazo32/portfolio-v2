945e8518121360c417686e96115f5661d1eef419
Lucas Mazo <lucasmazo32@gmail.com>
2020-07-16 16:10:02 -0500

    finished first part of the portfolio and made it responsive

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import { ReactComponent as Email } from '../assets/images/email.svg';
 import { ReactComponent as Phone } from '../assets/images/phone.svg';
+import { ReactComponent as Github } from '../assets/images/social/github.svg';
+import { ReactComponent as Twitter } from '../assets/images/social/twitter.svg';
+import { ReactComponent as Linkedin } from '../assets/images/social/linkedin.svg';
 import '../assets/styles/Presentation.scss';
 
 export default function Presentation() {
   return (
-    <section className="Presentation">
-      <h1>Hello, I am a Front end Developer</h1>
-      <p>Passionate about consumer-centric products and generating a great user experience.</p>
+    <section className="Presentation container-xl">
+      <div className="info-container">
+        <h1>
+          Hello, I am
+          <br />
+          {' '}
+          a Front end Developer
+        </h1>
+        <p>
+          Passionate about efficiency, consumer-centric products,
+          generating a great user experience.
+        </p>
+        <span>Are you a recruiter?&nbsp;&nbsp;&nbsp;</span>
+        <Button variant="contained">Click here</Button>
+      </div>
+      <div className="contact-container">
+        <h2>Contact me</h2>
+        <a className="a-text" href="tel: +57 3135364824">
+          <Phone />
+          {' '}
+          313 536 4824
+        </a>
+        <a className="a-text" href="mailto: lucasmazo32@gmail.com">
+          <Email />
+          {' '}
+          lucasmazo32@gmail.com
+        </a>
+        <div className="social">
+          <span>Social: </span>
+          <a className="icons github" href="https://github.com/lucasmazo32" aria-label="Github" target="_blank" rel="noopener noreferrer"><Github /></a>
+          <a className="icons linkedin" href="https://www.linkedin.com/in/lucasmazo/" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer"><Linkedin /></a>
+          <a className="icons twitter" href="https://twitter.com/lucasmazo32" aria-label="Twitter" target="_blank" rel="noopener noreferrer"><Twitter /></a>
+        </div>
+      </div>
     </section>
   );
 }
